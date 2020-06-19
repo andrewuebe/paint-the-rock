1267e7a502afbe3cd151108149b3449b151a039d
Andrew Uebelein <auebelei@skidmore.edu>
2020-06-19 14:20:12 -0500

    Try again to fix meta tags in head
    (added react-snapshot)

--- src/pages/ShareRock.js
+++ src/pages/ShareRock.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { render } from 'react-snapshot';
 import axios from "axios";
 import Konva from "konva";
 import { Stage, Layer } from "react-konva";
